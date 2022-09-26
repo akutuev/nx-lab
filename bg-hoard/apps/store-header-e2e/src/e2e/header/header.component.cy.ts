0a3fecd8493c148ddfad67563dc1d1ad9c21eb71
Alexander Kutuev <AKutuev@slb.com>
2022-09-26 15:32:14 +0100

    lab 10

---
+++ bg-hoard/apps/store-header-e2e/src/e2e/header/header.component.cy.ts
@@ -0,0 +1,8 @@
+describe('store-header', () => {
+  beforeEach(() =>
+    cy.visit('/iframe.html?id=headercomponent--primary&args=title;')
+  );
+  it('should render the component', () => {
+    cy.get('bg-hoard-header').should('exist');
+  });
+});
