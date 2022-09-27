399f0507485864c89e67310146ee11b3f7a0e3e8
Alexander Kutuev <AKutuev@slb.com>
2022-09-27 10:36:02 +0100

    update schema

---
+++ bg-hoard/tools/generators/update-scope-schema/index.ts
@@ -0,0 +1,18 @@
+import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
+import { libraryGenerator } from '@nrwl/workspace/generators';
+import { updateJson } from '@nrwl/devkit';
+
+export default async function (tree: Tree, schema: any) {
+  // await libraryGenerator(tree, {name: schema.name});
+  // await formatFiles(tree);
+
+  updateJson(tree, 'nx.json', (json): any => {
+    json.defaultProject = "api"
+
+    return json;
+  })
+
+  return () => {
+    installPackagesTask(tree);
+  };
+}
