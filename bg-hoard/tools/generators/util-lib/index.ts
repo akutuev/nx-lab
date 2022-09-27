84ded8957e93129443a90653fc3f2c9b0c1a3727
Alexander Kutuev <AKutuev@slb.com>
2022-09-27 09:53:36 +0100

    update generator

--- bg-hoard/tools/generators/util-lib/index.ts
+++ bg-hoard/tools/generators/util-lib/index.ts
@@ -2,7 +2,7 @@ import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
 export default async function (tree: Tree, schema: any) {
-  await libraryGenerator(tree, { name: schema.name });
+  await libraryGenerator(tree, { name: schema.name, directory: schema.directory, tags: schema.tags });
   await formatFiles(tree);
   return () => {
     installPackagesTask(tree);
